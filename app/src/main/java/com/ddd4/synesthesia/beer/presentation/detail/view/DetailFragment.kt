ed03bc86c0059f8fe5a8550a535d126283779484
Evan Song <evace93@gmail.com>
2020-08-09 15:05:25 +0900

    feat: 홈, 상세 fragment  및 navigation xml 추가

---
+++ app/src/main/java/com/ddd4/synesthesia/beer/presentation/detail/view/DetailFragment.kt
@@ -0,0 +1,9 @@
+package com.ddd4.synesthesia.beer.presentation.detail.view
+
+import com.ddd4.synesthesia.beer.R
+import com.ddd4.synesthesia.beer.base.BaseFragment
+import com.ddd4.synesthesia.beer.databinding.FragmentDetailBinding
+
+class DetailFragment : BaseFragment<FragmentDetailBinding>(R.layout.fragment_detail) {
+
+}
--- app/src/main/java/com/ddd4/synesthesia/beer/presentation/main/view/MainActivity.kt
+++ app/src/main/java/com/ddd4/synesthesia/beer/presentation/main/view/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.ddd4.synesthesia.beer.presentation.main.view
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import com.ddd4.synesthesia.beer.R
 import com.ddd4.synesthesia.beer.base.BaseActivity
@@ -8,6 +10,17 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    companion object {
+        fun start(context: Context) {
+            context.run {
+                startActivity(
+                    Intent(this, MainActivity::class.java)
+                )
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
