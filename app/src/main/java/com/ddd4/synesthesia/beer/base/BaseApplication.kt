e7bf5d7491a423014e9c6db9289a015322b1052d
Evan Song <evace93@gmail.com>
2020-08-14 23:27:28 +0900

    feat: home ui 추가, dummy data 추가

--- app/src/main/java/com/ddd4/synesthesia/beer/base/BaseApplication.kt
+++ app/src/main/java/com/ddd4/synesthesia/beer/base/BaseApplication.kt
@@ -1,11 +1,18 @@
 package com.ddd4.synesthesia.beer.base
 
 import android.app.Application
+import com.ddd4.synesthesia.beer.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class BaseApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+
     }
 }
