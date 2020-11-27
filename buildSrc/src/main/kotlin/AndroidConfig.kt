960f88c36a4b9ad9793f8d8d94296c9c061b96fe
hyojin <jin33032@gmail.com>
2020-11-27 23:56:40 +0900

    versionCode 4 / versionName 1.0.2

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -3,8 +3,8 @@ object AndroidConfig {
     const val MIN_SDK_VERSION = 23
     const val TARGET_SDK_VERSION = 29
 
-    const val VERSION_CODE = 3
-    const val VERSION_NAME = "1.0.1"
+    const val VERSION_CODE = 4
+    const val VERSION_NAME = "1.0.2"
 
     const val BUILD_TOOLS_VERSION = "29.0.3"
     const val APPLICATION_ID = "com.ddd4.synesthesia.beer"
