002d1b7c00d640acb501e79d69175276b4868d3d
hyojin <jin33032@gmail.com>
2021-01-24 23:05:20 +0900

    feat : target version 30 으로 변경 (#88)

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -1,12 +1,12 @@
 object AndroidConfig {
-    const val COMPILE_SDK_VERSION = 29
+    const val COMPILE_SDK_VERSION = 30
     const val MIN_SDK_VERSION = 23
-    const val TARGET_SDK_VERSION = 29
+    const val TARGET_SDK_VERSION = 30
 
     const val VERSION_CODE = 8
     const val VERSION_NAME = "1.2.1-dev"
 
-    const val BUILD_TOOLS_VERSION = "29.0.3"
+    const val BUILD_TOOLS_VERSION = "30.0.3"
     const val APPLICATION_ID = "com.ddd4.synesthesia.beer"
     const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
 }
