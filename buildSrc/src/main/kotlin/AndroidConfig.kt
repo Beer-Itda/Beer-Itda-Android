95023825b0ea1235426b28c37622c3188d49a17d
hyojin <jin33032@gmail.com>
2021-01-29 21:08:50 +0900

    versionCode 8 / versionName 1.2.1-rc01

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -4,7 +4,7 @@ object AndroidConfig {
     const val TARGET_SDK_VERSION = 30
 
     const val VERSION_CODE = 8
-    const val VERSION_NAME = "1.2.1-dev"
+    const val VERSION_NAME = "1.2.1-rc01"
 
     const val BUILD_TOOLS_VERSION = "30.0.3"
     const val APPLICATION_ID = "com.ddd4.synesthesia.beer"
