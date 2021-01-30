5faa3f4857751d8577f0a21455f5d5e722238de9
hyojin <jin33032@gmail.com>
2021-01-30 14:00:27 +0900

    versionCode 10 / versionName 2.0.0-dev

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -3,8 +3,8 @@ object AndroidConfig {
     const val MIN_SDK_VERSION = 23
     const val TARGET_SDK_VERSION = 30
 
-    const val VERSION_CODE = 9
-    const val VERSION_NAME = "1.2.2-dev"
+    const val VERSION_CODE = 10
+    const val VERSION_NAME = "2.0.0-dev"
 
     const val BUILD_TOOLS_VERSION = "30.0.3"
     const val APPLICATION_ID = "com.ddd4.synesthesia.beer"
