f1de109ef0e52b6a377574fe3407fd88452574fc
hyojin <jin33032@gmail.com>
2021-11-07 15:43:45 +0900

    feat : data module 추가

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -1,3 +1,5 @@
+import org.gradle.api.JavaVersion
+
 object AndroidConfig {
     const val COMPILE_SDK_VERSION = 30
     const val MIN_SDK_VERSION = 23
@@ -9,4 +11,6 @@ object AndroidConfig {
     const val BUILD_TOOLS_VERSION = "30.0.3"
     const val APPLICATION_ID = "com.ddd4.synesthesia.beer"
     const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
+    val JAVA_VERSION = JavaVersion.VERSION_11
+
 }
