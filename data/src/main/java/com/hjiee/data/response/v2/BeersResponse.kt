3716c8198b41b83da5892de60c502ea6f0ca2ca1
hyojin <jin33032@gmail.com>
2022-01-16 23:19:58 +0900

    BIA-11 맥주리스트 response key 변경

--- data/src/main/java/com/hjiee/data/response/v2/BeersResponse.kt
+++ data/src/main/java/com/hjiee/data/response/v2/BeersResponse.kt
@@ -3,7 +3,7 @@ package com.hjiee.data.response.v2
 import com.google.gson.annotations.SerializedName
 
 data class BeersResponse(
-    @SerializedName("beer")
+    @SerializedName("beers")
     val beers: List<BeerResponse>?,
     @SerializedName("cursor")
     val cursor: CursorResponse?
